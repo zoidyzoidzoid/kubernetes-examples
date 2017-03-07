13bbcdf24a213df99e74a9aeb2e5e3af1921de0b
William Stewart <zoidbergwill@gmail.com>
2017-03-07 09:47:29 +0200

    Simple hello world env var example

---
+++ scaleconf/hello-world-env-example/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func GetMessage() string {
+	value := os.Getenv("MESSAGE")
+	if len(value) == 0 {
+		return "World"
+	}
+	return value
+}
+
+func main() {
+	fmt.Println("hello-world: 0.0.1")
+	message := GetMessage()
+	fmt.Printf("Hello, %s!\n", message)
+}
